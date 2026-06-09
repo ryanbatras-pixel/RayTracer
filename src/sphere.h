514405623bcc5af23df6e8a440d93605fc90e5d3
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-09 23:49:45 +0530

    Add Sphere class with intersection method

--- src/sphere.h
+++ src/sphere.h
@@ -1,1 +1,31 @@
+#pragma once
 
+#include "ray.h"
+
+class Sphere {
+public:
+    Vec3 center;
+    double radius;
+    Vec3 color;
+
+    Sphere(Vec3 c,double r,Vec3 col)
+        : center(c), radius(r), color(col) {}
+
+    bool intersect(const Ray& ray,double& t) {
+
+        Vec3 oc=ray.origin-center;
+
+        double a=ray.direction.dot(ray.direction);
+        double b=2.0*oc.dot(ray.direction);
+        double c=oc.dot(oc)-radius*radius;
+
+        double discriminant=b*b-4*a*c;
+
+        if(discriminant<0)
+            return false;
+
+        t=(-b-sqrt(discriminant))/(2*a);
+
+        return t>0;
+    }
+};
