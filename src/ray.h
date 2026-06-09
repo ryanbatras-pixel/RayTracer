1a681e7cebcbba929cd731498e1687aa2bfb455a
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-09 23:49:16 +0530

    Refactor Ray class to use Vec3 instead of Vector

--- src/ray.h
+++ src/ray.h
@@ -1,20 +1,11 @@
-#ifndef RAY_H
-#define RAY_H
-
-#include "vector.h"
+#pragma once
+#include "vec3.h"
 
 class Ray {
 public:
-    Vector origin;
-    Vector direction;
+    Vec3 origin;
+    Vec3 direction;
 
-    Ray(const Vector& origin_, const Vector& direction_)
-        : origin(origin_), direction(direction_) {}
-
-    Vector point_at(double distance) const {
-        return origin + direction * distance;
-    }
+    Ray(Vec3 o, Vec3 d)
+        : origin(o), direction(d) {}
 };
-
-#endif
-
