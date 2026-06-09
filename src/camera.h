d6fd0beaa9dbf57e0d4eb5e2dd7358838a492bc4
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-09 23:50:10 +0530

    Add Camera class with ray generation method

--- src/camera.h
+++ src/camera.h
@@ -1,1 +1,19 @@
+#pragma once
 
+#include "ray.h"
+
+class Camera {
+public:
+    Vec3 position;
+
+    Camera()
+        : position(0,0,0) {}
+
+    Ray getRay(double u,double v) {
+
+        Vec3 viewport(u,v,-1);
+
+        return Ray(position,
+                   viewport.normalize());
+    }
+};
