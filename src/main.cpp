84e361689b5da5f80b9c9cf05956d4daae7bb454
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-09 23:50:59 +0530

    Add scene setup and rendering in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,32 @@
+#include <vector>
 
+#include "renderer.h"
+
+int main()
+{
+    std::vector<Sphere> scene;
+
+    scene.push_back(
+        Sphere(
+            Vec3(0,0,-5),
+            1,
+            Vec3(1,0,0)));
+
+    scene.push_back(
+        Sphere(
+            Vec3(2,0,-6),
+            1,
+            Vec3(0,1,0)));
+
+    scene.push_back(
+        Sphere(
+            Vec3(-2,0,-6),
+            1,
+            Vec3(0,0,1)));
+
+    Renderer renderer;
+
+    renderer.render(scene);
+
+    return 0;
+}
