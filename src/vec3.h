40d2a85ccfc6c6ebc88b21ae3c3aaded89c4c4c5
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-10 23:47:26 +0530

    Refactor Vec3 class to struct and improve formatting

--- src/vec3.h
+++ src/vec3.h
@@ -1,33 +1,35 @@
 #pragma once
 #include <cmath>
 
-class Vec3 {
-public:
-    double x,y,z;
+struct Vec3 {
+    double x, y, z;
 
     Vec3() : x(0), y(0), z(0) {}
-
-    Vec3(double x,double y,double z)
-        : x(x), y(y), z(z) {}
+    Vec3(double x_, double y_, double z_)
+        : x(x_), y(y_), z(z_) {}
 
     Vec3 operator+(const Vec3& v) const {
-        return Vec3(x+v.x,y+v.y,z+v.z);
+        return Vec3(x + v.x, y + v.y, z + v.z);
     }
 
     Vec3 operator-(const Vec3& v) const {
-        return Vec3(x-v.x,y-v.y,z-v.z);
+        return Vec3(x - v.x, y - v.y, z - v.z);
     }
 
     Vec3 operator*(double s) const {
-        return Vec3(x*s,y*s,z*s);
+        return Vec3(x * s, y * s, z * s);
     }
 
     double dot(const Vec3& v) const {
-        return x*v.x+y*v.y+z*v.z;
+        return x * v.x + y * v.y + z * v.z;
+    }
+
+    double length() const {
+        return std::sqrt(dot(*this));
     }
 
     Vec3 normalize() const {
-        double len=sqrt(dot(*this));
-        return (*this)*(1.0/len);
+        double len = length();
+        return Vec3(x / len, y / len, z / len);
     }
 };
