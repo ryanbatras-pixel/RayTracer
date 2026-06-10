c503f934d95eaaf45b92619fd7211fd375f3b74f
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-10 23:48:23 +0530

    Update sphere.h

--- src/sphere.h
+++ src/sphere.h
@@ -2,30 +2,22 @@
 
 #include "ray.h"
 
-class Sphere {
-public:
+struct Sphere {
     Vec3 center;
     double radius;
-    Vec3 color;
 
-    Sphere(Vec3 c,double r,Vec3 col)
-        : center(c), radius(r), color(col) {}
+    Sphere(const Vec3& c, double r)
+        : center(c), radius(r) {}
 
-    bool intersect(const Ray& ray,double& t) {
+    bool hit(const Ray& ray) const {
+        Vec3 oc = ray.origin - center;
 
-        Vec3 oc=ray.origin-center;
+        double a = ray.direction.dot(ray.direction);
+        double b = 2.0 * oc.dot(ray.direction);
+        double c = oc.dot(oc) - radius * radius;
 
-        double a=ray.direction.dot(ray.direction);
-        double b=2.0*oc.dot(ray.direction);
-        double c=oc.dot(oc)-radius*radius;
+        double discriminant = b * b - 4 * a * c;
 
-        double discriminant=b*b-4*a*c;
-
-        if(discriminant<0)
-            return false;
-
-        t=(-b-sqrt(discriminant))/(2*a);
-
-        return t>0;
+        return discriminant >= 0;
     }
 };
