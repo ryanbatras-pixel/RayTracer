1eecb08a49c0a90e44140ec0dc883ef58fbad97c
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-12 00:25:33 +0530

    Update scene.h

--- src/scene.h
+++ src/scene.h
@@ -4,48 +4,97 @@
 #include "material.h"
 #include "sphere.h"
 
-inline hittable_list impressive_scene() {
+inline hittable_list solar_system_scene() {
     hittable_list world;
 
-    auto ground = make_shared<lambertian>(color(0.45, 0.42, 0.34));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground));
+    auto space_floor = make_shared<lambertian>(
+        color(0.12, 0.12, 0.12));
 
-    auto glass = make_shared<dielectric>(1.5);
-    auto copper = make_shared<metal>(color(0.95, 0.58, 0.32), 0.18);
-    auto chrome = make_shared<metal>(color(0.82, 0.86, 0.90), 0.03);
-    auto blue = make_shared<lambertian>(color(0.12, 0.24, 0.60));
-    auto red = make_shared<lambertian>(color(0.75, 0.18, 0.12));
-    auto light = make_shared<diffuse_light>(color(5.5, 4.5, 3.2));
-    auto cyan_light = make_shared<diffuse_light>(color(1.0, 4.0, 5.5));
+    world.add(make_shared<sphere>(
+        point3(0, -1000, 0),
+        1000,
+        space_floor));
 
-    world.add(make_shared<sphere>(point3(-1.15, 1.0, -1.0), 1.0, glass));
-    world.add(make_shared<sphere>(point3(1.35, 0.9, -0.65), 0.9, copper));
-    world.add(make_shared<sphere>(point3(0.05, 0.55, 1.05), 0.55, red));
-    world.add(make_shared<sphere>(point3(-2.15, 0.42, 1.15), 0.42, chrome));
-    world.add(make_shared<sphere>(point3(2.45, 0.35, 0.75), 0.35, blue));
-    world.add(make_shared<sphere>(point3(-2.6, 2.1, -2.6), 0.45, light));
-    world.add(make_shared<sphere>(point3(2.7, 1.35, 1.1), 0.30, cyan_light));
+    auto sun = make_shared<diffuse_light>(
+        color(7.0, 6.5, 4.5));
 
-    for (int a = -6; a <= 6; a++) {
-        for (int b = -4; b <= 4; b++) {
-            auto choose_mat = random_double();
-            point3 center(a + 0.55 * random_double(), 0.16, b + 0.55 * random_double());
-            if ((center - point3(0, 0.16, 0)).length() < 1.25) continue;
+    auto mercury = make_shared<lambertian>(
+        color(0.55, 0.55, 0.55));
 
-            shared_ptr<material> sphere_material;
-            if (choose_mat < 0.58) {
-                auto albedo = color::random(0.18, 0.85) * color::random(0.18, 0.85);
-                sphere_material = make_shared<lambertian>(albedo);
-            } else if (choose_mat < 0.86) {
-                auto albedo = color::random(0.55, 1.0);
-                auto fuzz = random_double(0.0, 0.35);
-                sphere_material = make_shared<metal>(albedo, fuzz);
-            } else {
-                sphere_material = make_shared<dielectric>(1.5);
-            }
+    auto venus = make_shared<lambertian>(
+        color(0.90, 0.75, 0.35));
 
-            world.add(make_shared<sphere>(center, 0.16, sphere_material));
-        }
+    auto earth = make_shared<lambertian>(
+        color(0.20, 0.45, 1.00));
+
+    auto moon = make_shared<lambertian>(
+        color(0.80, 0.80, 0.80));
+
+    auto mars = make_shared<lambertian>(
+        color(0.80, 0.30, 0.20));
+
+    auto jupiter = make_shared<lambertian>(
+        color(0.90, 0.70, 0.45));
+
+    auto saturn = make_shared<lambertian>(
+        color(0.95, 0.85, 0.55));
+
+    auto ring = make_shared<metal>(
+        color(0.85, 0.85, 0.85),
+        0.05);
+
+    world.add(make_shared<sphere>(
+        point3(0, 2.5, 0),
+        2.0,
+        sun));
+
+    world.add(make_shared<sphere>(
+        point3(4, 0.2, 0),
+        0.15,
+        mercury));
+
+    world.add(make_shared<sphere>(
+        point3(6, 0.3, 0),
+        0.30,
+        venus));
+
+    world.add(make_shared<sphere>(
+        point3(8, 0.35, 0),
+        0.35,
+        earth));
+
+    world.add(make_shared<sphere>(
+        point3(8.7, 0.35, 0),
+        0.08,
+        moon));
+
+    world.add(make_shared<sphere>(
+        point3(10, 0.25, 0),
+        0.25,
+        mars));
+
+    world.add(make_shared<sphere>(
+        point3(14, 1.0, 0),
+        1.0,
+        jupiter));
+
+    world.add(make_shared<sphere>(
+        point3(19, 0.9, 0),
+        0.9,
+        saturn));
+
+    for (int i = 0; i < 60; i++) {
+        double angle = 2 * pi * i / 60.0;
+        double radius = 1.6;
+
+        world.add(make_shared<sphere>(
+            point3(
+                19 + radius * std::cos(angle),
+                0.9,
+                radius * std::sin(angle)
+            ),
+            0.05,
+            ring));
     }
 
     return world;
