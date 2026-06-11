de147071d10d476d671cdd29ac471925eadea7ae
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-11 23:55:10 +0530

    Add material classes for rendering

---
+++ src/material.h
@@ -0,0 +1,99 @@
+#ifndef MATERIAL_H
+#define MATERIAL_H
+
+#include "hittable.h"
+
+class material {
+public:
+    virtual ~material() = default;
+
+    virtual color emitted() const {
+        return color(0, 0, 0);
+    }
+
+    virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
+};
+
+class lambertian : public material {
+public:
+    explicit lambertian(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray&, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        if (scatter_direction.near_zero()) scatter_direction = rec.normal;
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+private:
+    color albedo;
+};
+
+class metal : public material {
+public:
+    metal(const color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
+        attenuation = albedo;
+        return dot(scattered.direction(), rec.normal) > 0;
+    }
+
+private:
+    color albedo;
+    double fuzz;
+};
+
+class dielectric : public material {
+public:
+    explicit dielectric(double refraction_index) : ir(refraction_index) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+        double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
+            direction = reflect(unit_direction, rec.normal);
+        else
+            direction = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, direction);
+        return true;
+    }
+
+private:
+    double ir;
+
+    static double reflectance(double cosine, double ref_idx) {
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * std::pow((1 - cosine), 5);
+    }
+};
+
+class diffuse_light : public material {
+public:
+    explicit diffuse_light(color emit) : emit(emit) {}
+
+    color emitted() const override {
+        return emit;
+    }
+
+    bool scatter(const ray&, const hit_record&, color&, ray&) const override {
+        return false;
+    }
+
+private:
+    color emit;
+};
+
+#endif
