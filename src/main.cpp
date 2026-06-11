376e01d6ee2db9b8ec7eb2c7548a44b482fe3075
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-12 00:25:12 +0530

    Modify camera settings for solar system scene

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,34 @@
 #include "scene.h"
 
 int main() {
-    auto world = impressive_scene();
+
+    auto world = solar_system_scene();
 
     camera cam;
+
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 700;
-    cam.samples_per_pixel = 80;
+
+    cam.image_width = 1200;
+
+    cam.samples_per_pixel = 100;
+
     cam.max_depth = 20;
-    cam.background = color(0.012, 0.014, 0.020);
 
-    cam.vfov = 36;
-    cam.lookfrom = point3(5.2, 2.7, 5.6);
-    cam.lookat = point3(0.1, 0.7, -0.15);
+    cam.background = color(0.01, 0.01, 0.03);
+
+    cam.vfov = 25;
+
+    cam.lookfrom = point3(26, 8, 18);
+
+    cam.lookat = point3(8, 1, 0);
+
     cam.vup = vec3(0, 1, 0);
-    cam.defocus_angle = 0.35;
-    cam.focus_dist = 6.8;
 
-    cam.render(world, "render.ppm");
+    cam.defocus_angle = 0.1;
+
+    cam.focus_dist = 25.0;
+
+    cam.render(world, "solar_system.ppm");
+
     return 0;
 }
