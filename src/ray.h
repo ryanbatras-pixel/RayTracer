a9e6f2657cfa581cff071b3842b02ff440490161
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-11 23:46:00 +0530

    Refactor Ray struct to ray class with new methods and more versatile code

--- src/ray.h
+++ src/ray.h
@@ -1,11 +1,23 @@
-#pragma once
+#ifndef RAY_H
+#define RAY_H
 
 #include "vec3.h"
 
-struct Ray {
-    Vec3 origin;
-    Vec3 direction;
+class ray {
+public:
+    ray() {}
+    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-    Ray(const Vec3& o, const Vec3& d)
-        : origin(o), direction(d) {}
+    point3 origin() const { return orig; }
+    vec3 direction() const { return dir; }
+
+    point3 at(double t) const {
+        return orig + t * dir;
+    }
+
+private:
+    point3 orig;
+    vec3 dir;
 };
+
+#endif
