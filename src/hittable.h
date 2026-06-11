258abdc091f118e2fe044084c6eed5a85e294f7a
ryanbatras-pixel <ryanbatras@gmail.com>
2026-06-11 23:52:02 +0530

    Add hittable interface and hittable_list class

---
+++ src/hittable.h
@@ -0,0 +1,56 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+#include <vector>
+
+class material;
+
+struct hit_record {
+    point3 p;
+    vec3 normal;
+    shared_ptr<material> mat;
+    double t;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+};
+
+class hittable {
+public:
+    virtual ~hittable() = default;
+    virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const = 0;
+};
+
+class hittable_list : public hittable {
+public:
+    std::vector<shared_ptr<hittable>> objects;
+
+    hittable_list() {}
+    explicit hittable_list(shared_ptr<hittable> object) { add(object); }
+
+    void clear() { objects.clear(); }
+    void add(shared_ptr<hittable> object) { objects.push_back(object); }
+
+    bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        auto closest_so_far = t_max;
+
+        for (const auto& object : objects) {
+            if (object->hit(r, t_min, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
+#endif
